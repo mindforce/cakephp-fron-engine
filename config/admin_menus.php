d36d97b65148d02b83fc23635689f74cbed355b9
Denys Kyselov <denys.kyselov@gmail.com>
2018-05-03 13:10:09 +0300

    Typo fix

--- config/admin_menus.php
+++ config/admin_menus.php
@@ -8,22 +8,22 @@ $config = [
                         'title' => __d('front_engine', 'Lookout'),
                         'weight' => 40,
                         'options' => [
-							'icon' => 'fa fa-leaf'
-						],
+                            'icon' => 'fa fa-leaf'
+                        ],
                         'children' => [
                             'menus' => [
-								'title' => __d('front_engine', 'Menus'),
+                                'title' => __d('front_engine', 'Menus'),
                                 'url' => [
                                     'prefix' => 'admin',
                                     'plugin' => 'FrontEngine',
                                     'controller' => 'Menus',
                                     'action' => 'index'
                                 ],
-								'weight' => 40,
-								'options' => [
-									'icon' => 'fa fa-bars'
-								]
-							]
+                                'weight' => 40,
+                                'options' => [
+                                    'icon' => 'fa fa-bars'
+                                ]
+                            ]
                         ]
                     ]
                 ]
